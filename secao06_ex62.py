07d9064f482fabc04b8eff9b7f8b3cf99690d1b0
Israel Philip Linhares <93551089+IsraelLinhares@users.noreply.github.com>
2022-03-10 17:53:54 -0300

    seção 06 terminada

--- secao06_ex62.py
+++ secao06_ex62.py
@@ -1,9 +1,46 @@
-unidad = ['zero','um','dois','treis','quatro','cinco','seis','sete','oito','nove']
-dezeAlg = ['onze','doze','treze','quatorze','quinze','dezeseis','dezesete','dezoito','deznove']
-dezenAll = ['dez','vinte','trinta','quarenta','cinquenta','sessenta','setenta','oitenta','noventa']
-cent = ['cem','duzentos','trezetos','quatroscentos','quinhentos','seiscentos','setecentos','oitocentos','novecentos']
-lista = []
-for i in range(1,1001):
-    if i <= 9:
-        lista.append(unidad[i+1])
-        
+
+
+unidad_list = ['um','dois','tres','quatro','cinco','seis','sete','oito','nove']
+um_dezenove_list = ['um','dois','tres','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove']
+dezen_list = ['vinte','trinta','quarenta','cinquenta','sessenta','setenta','oitenta','noventa']
+cent_list = ['cento','duzentos','trezentos','quatrocentos','quinhentos','seiscentos','setecentos','oitocentos','novecentos']
+soma = ''
+
+#um até dezenove
+for i in range(len(um_dezenove_list)):
+    soma += um_dezenove_list[i]
+
+#dezenas apenas(20,90)
+for i in range(len(dezen_list)):
+    soma += dezen_list[i]
+
+  
+#dezenas e unidades
+for i in range(len(dezen_list)):
+    for i2 in range(len(unidad_list)):
+        soma+=f'{dezen_list[i]}e{unidad_list[i2]}'
+
+ #centenas com 1 até 19
+for i in range(len(cent_list)):
+    for i2 in range(len(um_dezenove_list)):
+        soma+=f'{cent_list[i]}e{um_dezenove_list[i2]}'
+
+#centenas e dezenas apenas
+for i in range(len(cent_list)):
+    for i2 in range(len(dezen_list)):
+        soma+=f'{cent_list[i]}e{dezen_list[i2]}'
+
+#centenas , dezenas e unidades (121, 999)
+for i in range(len(cent_list)):
+    for i2 in range(len(dezen_list)):
+        for i3 in range(len(unidad_list)):
+            soma+=f'{cent_list[i]}e{dezen_list[i2]}e{unidad_list[i3]}'
+              
+soma+='cemmil'
+
+
+
+    
+print(len(soma))
+
+
--- testes.py
+++
@@ -1,8 +0,0 @@
-unidad = ['','um','dois','treis','quatro','cinco','seis','sete','oito','nove']
-lista = []
-i = 0
-lista.append(unidad[i+1])
-lista.append(unidad[i+2])
-lista.append(unidad[i+3])
-lista.append(unidad[i+4])
-print(lista[0:4])
