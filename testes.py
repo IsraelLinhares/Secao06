ce89d73990222a3a2274a4e90702026fee3b66a5
Israel Philip Linhares <93551089+IsraelLinhares@users.noreply.github.com>
2022-03-28 17:58:33 -0300

    testes

--- testes.py
+++ testes.py
@@ -4,27 +4,28 @@ unidad_list = ['um','dois','tres','quatro','cinco','seis','sete','oito','nove']
 um_dezenove_list = ['um','dois','tres','quatro','cinco','seis','sete','oito','nove','','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove']
 dezen_list = ['dez','vinte','trinta','quarenta','cinquenta','sessenta','setenta','oitenta','noventa']
 cent_list = ['cento','duzentos','trezentos','quatrocentos','quinhentos','seiscentos','setecentos','oitocentos','novecentos']
-soma = ''
 
 lista = []
 dezen_count = 0
 Unidad_count = -1
 cent_count = 0
+a = 0
 for i in range(1,999):
     Unidad_count +=1
-    if i >= 100 and i%100 == 0:
+    if i >= 101:
+        a = str(i)
+        a = int(a[1:])
+
+
+    if i%100 == 0:
         lista.append(lista_centenas[cent_count])
         cent_count += 1
 
-    if i >= 101 and i <= 119:
+
+    if a >= 1 and a <= 19:
 
 
-
-        num = str(i)
-        num = int(num[1:])
-
-        lista.append(f'{cent_list[cent_count-1]}e')
-
+        lista.append(f'{cent_list[cent_count]}e')
 
 
     if i < 99 and i%10 == 0:
--- testes02.py
+++ testes02.py
@@ -1,5 +1,19 @@
-i = 119
 
-dezena = str(i)
-dezena = int(dezena[1:])
-print(dezena)
+
+lista_centenas = ['cem','duzentos','trezentos','quatrocentos','quinhentos','seiscentos','setecentos','oitocentos','novecentos']
+unidad_list = ['um','dois','tres','quatro','cinco','seis','sete','oito','nove']
+um_dezenove_list = ['um','dois','tres','quatro','cinco','seis','sete','oito','nove','','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove']
+dezen_list = ['dez','vinte','trinta','quarenta','cinquenta','sessenta','setenta','oitenta','noventa']
+cent_list = ['cento','duzentos','trezentos','quatrocentos','quinhentos','seiscentos','setecentos','oitocentos','novecentos']
+lista = []
+a = 0
+cent_count = 0
+for i in range(1,999):
+    if i%100 == 0:
+        cent_count+=1
+    if i >= 101:
+        a = str(i)
+        a = int(a[1:])
+    if a >= 1 and a <= 19:
+        lista.append(f'{cent_list[cent_count]}e{um_dezenove_list[a-1]}')
+print(lista)
